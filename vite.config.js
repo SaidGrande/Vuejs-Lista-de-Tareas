3a7472018de6fac47cfd2ddc6fb02b3f3d90f4b0
SaidGrande <said.hernandez5155@alumnos.udg.mx>
2024-10-02 09:33:21 -0600

    Final v.4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: process.env.PORT || 3000, // Usa el puerto 3000 localmente o el que asigne Railway
+    host: true, // Permite acceder a la app desde la red
+  },
 })
